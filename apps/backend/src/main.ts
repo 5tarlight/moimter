b0f7755b5028b4417965e15812301e02d1d141a0
YEAHx4 <developerstarlight@gmail.com>
2025-07-09 16:44:43 +0900

    feat: startup with configured port

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigType } from '@nestjs/config';
+import appConfig from './config/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = app.get<ConfigType<typeof appConfig>>(appConfig.KEY);
+  const port = config.port;
+
+  await app.listen(port);
 }
 bootstrap();
