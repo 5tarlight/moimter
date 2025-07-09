c0fe2e2730fdc8b0374d773d2dd4169487ef4e62
YEAHx4 <developerstarlight@gmail.com>
2025-07-09 16:32:59 +0900

    feat: backend configuration

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config';
+import dbConfig from './config/db.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       cache: true,
       isGlobal: true,
+      load: [appConfig, dbConfig],
     }),
   ],
   controllers: [AppController],
--- apps/backend/src/app.service.ts
+++ apps/backend/src/app.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import appConfig from './config/app.config';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject(appConfig.KEY)
+    private readonly config: ConfigType<typeof appConfig>,
+  ) {}
+
   getHello(): string {
-    return 'Hello World!';
+    const port = this.config.port;
+    return `Running on port ${port}`;
   }
 }
