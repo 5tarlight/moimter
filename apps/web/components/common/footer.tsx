2126ab0d35b1f00bbaa4569a8840879e50ac5762
YEAHx4 <developerstarlight@gmail.com>
2025-07-16 15:47:40 +0900

    feat: footer layout

--- apps/web/components/common/footer.tsx
+++ apps/web/components/common/footer.tsx
@@ -2,13 +2,16 @@ import ContentWrapper from "./content-wrapper";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-4 h-64 flex justify-center">
+    <footer className="bg-gray-800 text-white py-8 flex justify-center">
       <ContentWrapper className="max-w-7xl h-full flex flex-col justify-between">
         <div className="flex justify-between">
-          <div>Pravacy</div>
-          <div>SNS</div>
+          <div>Moimter</div>
+          <div>기능</div>
+          <div>커뮤니티</div>
+          <div>고객지원</div>
         </div>
-        <p className="text-sm">
+        <hr className="my-8 text-white/20" />
+        <p className="text-center text-white/60">
           &copy; {new Date().getFullYear()} Moimter. All rights reserved.
         </p>
       </ContentWrapper>
