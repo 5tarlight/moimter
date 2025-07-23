37cfcd4611ed0c66db0979b7fa7332fcaa689b15
YEAHx4 <developerstarlight@gmail.com>
2025-07-23 17:02:35 +0900

    feat: auth bypass decorator

--- apps/backend/src/app.service.ts
+++ apps/backend/src/app.service.ts
@@ -1,19 +1,10 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { ConfigType } from '@nestjs/config';
-import appConfig from './config/app.config';
-import { DbService } from './db/db.service';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  constructor(
-    @Inject(appConfig.KEY)
-    private readonly config: ConfigType<typeof appConfig>,
-    private readonly dbService: DbService,
-  ) {}
+  constructor() {}
 
   getHello(): string {
-    const port = this.config.port;
-    const dbUrl = this.dbService.getUrl();
-    return `Running on port ${port}\nDatabase URL: ${dbUrl}`;
+    return 'Hello World!';
   }
 }
--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -1,13 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Module, SetMetadata } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth.guard';
+
+export const IS_PUBLIC_KEY = 'isPublic';
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
